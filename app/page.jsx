1ad08866a90ced39125bfd544cc19b8ed37d0b3b
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-06 13:05:25 +0600

    Homepage Banner Sart

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
 const Home = () => {
   return (
-    <div>homepage</div>
-  )
-}
+    <section className="h-ful">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text  */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Rakib Alom</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+          </div>
 
-export default Home
+          {/* photo  */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
