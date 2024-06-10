5622a1f7bfc234294c0acb35bebf925e27dd88d1
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-10 15:51:06 +0600

    work slider button updated and work page completed.

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,7 +1,20 @@
 "use client";
 
-const WorkSliderBtns = () => {
-  return <div>WorkSliderBtns</div>;
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderBtns = ({ containerStyles, btnStyles, incosStyels }) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={incosStyels} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={incosStyels} />
+      </button>
+    </div>
+  );
 };
 
 export default WorkSliderBtns;
