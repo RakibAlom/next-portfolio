b11c9e714d90e1218b5d53dd87e2ef44140cfb74
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-10 15:53:24 +0600

    contact page coding started...

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,21 @@
-const Contact = () => {
-  return (
-    <div>contact page</div>
-  )
-}
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default Contact
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+
+const Contact = () => {
+  return <div>contact page</div>;
+};
+
+export default Contact;
