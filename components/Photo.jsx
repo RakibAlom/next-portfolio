22ce5dca4242c5a1bbc28555ce7d35d4ca87ca7c
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-12 15:42:48 +0600

    Image alt updated

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -27,7 +27,7 @@ const Photo = () => {
             priority
             quality={100}
             fill
-            alt=""
+            alt="Rakib Alom"
             className="object-contain"
           />
         </motion.div>
